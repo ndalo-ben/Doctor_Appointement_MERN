8be59ff5da7a0c7bbc6939c7d1e3555ff7922f0d
ben <ndaloben8@gmail.com>
2023-12-19 13:41:50 +0300

    register page styling

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Form, Input } from 'antd'
+import "../styles/RegisterStyles.css"
+import { Link, useNavigate } from 'react-router-dom'
+
 
 const Register = () => {
+
+    const navigate = useNavigate()
 
     // form handler 
     const onFinishHandler = (values) => {
@@ -10,7 +15,7 @@ const Register = () => {
     return (
         <>
             <div className='form-container'>
-                <Form layout='vertical' onFinish={onFinishHandler}>
+                <Form layout='vertical' onFinish={onFinishHandler} className='register-form'>
                     <h1>Register</h1>
                     <Form.Item label='First Name' name='firstName' rules={[{ required: true, message: 'Please input your first name!' }]}>
                         <Input type='text' placeholder='First Name' required />
@@ -21,6 +26,7 @@ const Register = () => {
                     <Form.Item label='Password' name='password' rules={[{ required: true, message: 'Please input password!' }]}>
                         <Input type='password' placeholder='Password' required />
                     </Form.Item>
+                    <Link to={"/login"} className='m-2'>Already a user? Login</Link>
                     <button className='btn btn-primary' type='submit'>Register</button>
                 </Form>
             </div>
